e78733d0c88a473f69abcd23a579512837ef7990
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-08-01 16:03:11 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/elite_real_estate_posbackend/HelloServlet.java
@@ -0,0 +1,15 @@
+package lk.ijse.elite_real_estate_posbackend;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void init() {
+        String message = "Hello World!";
+    }
+
+    public void destroy() {
+    }
+}
